481892129745adc773bd2d35fb0327d6347cfdc1
Юрий Савенков <yura-savenkov1@yandex.ru>
2020-10-05 19:59:35 +0300

    opening mobile nav

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,7 +1,12 @@
 const mainNav = document.querySelector('.main-nav')
 const btnClose = document.querySelector('.main-nav__button--close')
+const btnOpen = document.querySelector('.main-nav__button--open')
 
-btnClose.addEventListener('click', evt => {
-    mainNav.classList.add('main-nav--closed');
+btnClose.addEventListener('click', () => {
+    mainNav.classList.add('main-nav--closed')
 })  
+
+btnOpen.addEventListener('click', () => {
+    mainNav.classList.remove('main-nav--closed')
+} )
 
