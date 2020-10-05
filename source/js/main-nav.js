0b80be6ae6be212237c2f2e02d7fa2d79076338e
Юрий Савенков <yura-savenkov1@yandex.ru>
2020-10-05 14:15:03 +0300

    toggle mobile menu

---
+++ source/js/main-nav.js
@@ -0,0 +1,7 @@
+const mainNav = document.querySelector('.main-nav')
+const btnClose = document.querySelector('.main-nav__button--close')
+
+btnClose.addEventListener('click', evt => {
+    mainNav.innerHTML = 'none';
+})
+
